c7c3a32abcef534a7f353ddf005df394f8f2bdb6
nyljeg <145918807+nyljeg@users.noreply.github.com>
2023-11-03 08:44:46 +0800

    Update jegbytes.lua

--- jegbytes.lua
+++ jegbytes.lua
@@ -108,11 +108,7 @@ Name = "Potions",
 Icon="rbxassetid://4483345998",
 PremiumOnly=false});
 
-local tot=v1:MakeTab({
-Name = "Halloween Event",
-Icon="rbxassetid://4483345998",
-PremiumOnly=false});
-	
+
 	
 --Toogle
 crate:AddDropdown({
@@ -643,41 +639,6 @@ pot:AddToggle({
   end    
 })
 
-tot:AddToggle({
-   Name = "Auto Trick Or Treat",
-   Default = false,
-   Callback = function(Value)
-     _G._ToT = Value
-	while wait() do
-		if _G._ToT == false then break end
-		  game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.7").knit.Services.TrickOrTreatService.RE.onTrickOrTreat:FireServer(i)
-	end
-  end    
-})
-
-tot:AddToggle({
-   Name = "Auto Ghost / Pedestrian",
-   Default = false,
-   Callback = function(Value)
-     _G._demon = Value
-	while wait() do
-		if _G._demon == false then break end
-		  game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.7").knit.Services.PedestrianService.RE.onDamagePedestrian:FireServer(i)
-	end
-  end    
-})
-
-tot:AddToggle({
-   Name = "Auto Pumpkin",
-   Default = false,
-   Callback = function(Value)
-     _G._pumpkin = Value
-	while wait() do
-		if _G._pumpkin == false then break end
-		  game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.7").knit.Services.PumpkinService.RE.onDamagePumpkin:FireServer(i)
-	end
-  end
-})
 
 	
 
