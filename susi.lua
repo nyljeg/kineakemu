749a5ff86a04fe6d4a8c9b4cb40e4a6e78c7b3b2
nyljeg <145918807+nyljeg@users.noreply.github.com>
2023-11-10 13:57:19 +0800

    Update susi.lua

--- susi.lua
+++ susi.lua
@@ -111,5 +111,5 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/nyljeg/kineakemu/main
 end)
 
 GetKeyButton.MouseButton1Click:Connect(function()
- setclipboard("https://raw.githubusercontent.com/nyljeg/kineakemu/main/sample.lua") 
+ setclipboard("https://link-center.net/1003806/download-elmerhub-key1") 
 end) 
