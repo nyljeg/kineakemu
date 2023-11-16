78a5f743c90bbcbe2676dc2f3691121bd4c5ec7b
jegbym19 <145918807+nyljeg@users.noreply.github.com>
2023-11-16 11:26:24 +0800

    Update jegbytes.lua

--- jegbytes.lua
+++ jegbytes.lua
@@ -698,6 +698,40 @@ game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit
 end 
 end
 });
+
+local dumbellslist = {"1Kg","2Kg","3Kg","4Kg","5Kg","10Kg","15Kg","20Kg","25Kg","50Kg","100Kg","250Kg","300Kg","400Kg","500Kg","650Kg","800Kg","1000Kg","1500Kg","2000Kg","2500kg","300Kg","3500Kg","4000Kg","5000Kg","6000Kg","7500Kg","10000Kg","12500Kg","15000Kg","20000Kg","25000Kg","30000Kg","35000Kg","40000Kg","45000Kg","50000Kg","60000Kg","70000Kg","80000Kg","90000Kg","100000Kg","125000Kg","150000Kg","175000Kg","200000Kg","250000Kg","300000Kg","350000Kg","375000Kg","400000Kg","425000Kg","450000Kg","475000Kg","500000Kg","525000Kg","550000Kg","575000Kg","600000Kg","625000Kg","650000Kg","675000Kg","700000Kg","725000Kg","750000Kg","775000Kg","800000Kg","825000Kg","850000Kg","875000Kg","900000Kg","925000Kg"}
+v59:AddLabel("Dumbells");
+v59:AddDropdown({
+   Name = "Zone",
+   Default = "1",
+   Options = {"1","2","3","4","5","6","7"},
+   Callback = function(Value)
+     _G.dumbzone = Value
+   end    
+});
+
+v59:AddDropdown({
+   Name = "Weights",
+   Default = "1Kg",
+   Options = dumbellslist,
+   Callback = function(Value)
+     _G.dumbtier = Value
+   end    
+});
+
+v59:AddToggle({
+   Name = "Equip Dumbell",
+   Default = false,
+   Callback = function(Value)
+     lump = Value
+	while wait() do
+    if lump == false then break end
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RE"):WaitForChild("onGuiEquipRequest"):FireServer(_G.dumbzone,"Dumbells",_G.dumbtier)
+	end
+  end    
+});
+
+	
 	
 v59:AddDropdown({
 Name = "Weights",
