aa2845b349f9164ddb31527dd10d7c83b69080d6
jegbytes <145918807+nyljeg@users.noreply.github.com>
2023-11-22 13:15:53 +0800

    Update jegbytes.lua

--- jegbytes.lua
+++ jegbytes.lua
@@ -1,36 +1,3 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-
-setclipboard("https://discord.gg/y2WxeCN3")
-toclipboard("https://discord.gg/y2WxeCN3")
-
-local Window = Rayfield:CreateWindow({
-   Name = "KEY SYSTEM",
-   LoadingTitle = "Load Key System",
-   LoadingSubtitle = "The link key has been copied on your keyboard",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "ElmerHub"
-   },
-   Discord = {
-      Enabled = true,
-      Invite = "y2WxeCN3", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = false -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "ElmerHub Key System",
-      Subtitle = "Key System",
-      Note = "Key has been copied on the keyboard",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://pastebin.com/raw/MLnK77Nn"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
-
-Rayfield:Destroy()
-
 local msg = Instance.new("Message",workspace)
 msg.Text = "Made By Elmer Mangio"
 wait(5.8)
@@ -645,8 +612,7 @@ v58:AddToggle({
   Callback = function(Value)
   _G.Gift = Value
     while wait() do
-      if _G.Gift == false then break end
-game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer("PAK U ALL","All")					
+      if _G.Gift == false then break end					
 game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("TimedRewardService"):WaitForChild("RE"):WaitForChild("onClaim"):FireServer("1")
 game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("TimedRewardService"):WaitForChild("RE"):WaitForChild("onClaim"):FireServer("2")
 game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("TimedRewardService"):WaitForChild("RE"):WaitForChild("onClaim"):FireServer("3")
