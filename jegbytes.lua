e0cd2557860c2ecc3ff525c4239ddd38ea9964f2
jegbytes <145918807+nyljeg@users.noreply.github.com>
2023-12-05 05:31:25 +0800

    Update jegbytes.lua

--- jegbytes.lua
+++ jegbytes.lua
@@ -12,7 +12,7 @@ SaveConfig=true,
 ConfigFolder="nyljegLib"});
 
 
---[[
+
 _G.AutoDeleteWithTable = {
    Earth = nil,
    Icy = nil,
@@ -70,7 +70,7 @@ mt.__namecall = newcclosure(function(self, ...)
 end
 	return namecall(self, ...) 
 end)
-	]]--
+	
 ------------------
 
 --Main Tab
@@ -348,8 +348,8 @@ v58:AddToggle({
   end
   end    
 })
---[[
-local dislist = {"=[ Zone 1 ]=","Earth","Icy","Blackhole","Lava","=[ Zone 2 ]=","Molten","Crystal","Solar","Ice","Burning","Moon","=[ Zone 3 ]=","Coconut","Palm","Treasure","Poseidon","KingFish","Clam","=[ Zone 4 ]=","Rust","Widget","Atom","Nuclear","Mutant","Iridescent","=[ Zone 5 ]=","TRex","Herbivore","Pterodactyl","Gem","DinoFossil","Mystic","=[ Zone 6 ]=","Void","Nebula","Wormhole","Star","=[ ZONE 7 ]=","Meteor","Cyberpunk","Deepsea","Rocket","=[ Aqua Event ]=","Shark","Crab","Jellyfish","=[ Limited Egg ]=","Candy"}
+
+local dislist = {"=[ Zone 1 ]=","Earth","Icy","Blackhole","Lava","=[ Zone 2 ]=","Molten","Crystal","Solar","Ice","Burning","Moon","=[ Zone 3 ]=","Coconut","Palm","Treasure","Poseidon","KingFish","Clam","=[ Zone 4 ]=","Rust","Widget","Atom","Nuclear","Mutant","Iridescent","=[ Zone 5 ]=","TRex","Herbivore","Pterodactyl","Gem","DinoFossil","Mystic","=[ Zone 6 ]=","Void","Nebula","Wormhole","Star","=[ ZONE 7 ]=","Meteor","Cyberpunk","Deepsea","Rocket","=[ Aqua Event ]=","Shark","Crab","Jellyfish","=[ Holloween ]=","Tomb","Ectoplasmic","Bewitched","Cauldron","=[ Event Egg ]=","OldEvent","Event","=[ Limited Egg ]=","Limited1","Limited2","Candy","=[ Season Egg ]=","SeasonEgg"}
 v59:AddDropdown({
    Name = "Select EGG",
    Default = "Earth",
@@ -376,12 +376,9 @@ v59:AddToggle({
   end    
 })
 	
-]]--
+
 	--[[
 local egglist = {"Earth","Icy","Blackhole","Lava","Molten","Crystal","Solar","Ice","Burning","Moon","Coconut","Palm","Treasure","Poseidon","KingFish","Clam","Rust","Widget","Atom","Nuclear","Mutant","Iridescent","TRex","Herbivore","Pterodactyl","Gem","DinoFossil","Mystic","Void","Nebula","Wormhole","star","Meteor","Cyberpunk","Deepsea","Rocket"}
-]]--
-	
-AddTable(ReplicatedStorage.Eggs,egg)
 v59:AddDropdown({
    Name = "Choose your Egg",
    Default = "Earth",
@@ -407,6 +404,7 @@ game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit
 end 
 end
 });
+	]]--
 	
 tools:AddDropdown({
    Name = "Your Zone",
@@ -417,7 +415,7 @@ tools:AddDropdown({
    end    
 });
 
-tools:AddDropdown({
+tols:AddDropdown({
    Name = "Choose your Tier",
    Default = "Tier1",
    Options =  {"=[ ZONE 1 ]=","Tier1","Tier2","Tier3","=[ ZONE 2 ]=","Tier4","Tier5","Tier6","=[ ZONE 3 ]=","Tier7","Tier8","Tier9","=[ ZONE 4 ]=","Tier10","Tier11","tier12","=[ ZONE 6 ]=","Tier16","Tier17","Tier18","=[ ZONE 7 ]=","Tier19","Tier20","Tier21"},
@@ -483,7 +481,6 @@ end});
 
 local workspace = game:GetService("Workspace")
 local RunService = game:GetService("RunService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local zone = {}
 local VSPLAYER = {}
 local egg = {}
