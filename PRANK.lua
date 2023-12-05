a6cb78de1df98548d0bc061e9dab9b3011ddc2cc
jegbytes <145918807+nyljeg@users.noreply.github.com>
2023-12-05 14:42:49 +0800

    Update PRANK.lua

--- PRANK.lua
+++ PRANK.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl...)
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))();
 local Player = game.Players.LocalPlayer
 local Window = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, SaveConfig = true, IntroEnabled = false})
 
