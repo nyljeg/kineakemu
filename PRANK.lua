ce10ca3938c878af6c1734f5bd70f474f110c4c6
jegbytes <145918807+nyljeg@users.noreply.github.com>
2023-12-05 14:31:28 +0800

    Add files via upload

---
+++ PRANK.lua
@@ -0,0 +1,59 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl...)
+local Player = game.Players.LocalPlayer
+local Window = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, SaveConfig = true, IntroEnabled = false})
+
+OrionLib:MakeNotification({
+ Name = "logged in!",
+ Content = "You are logged in as "..Player.Name.." ",
+ Image = "rbxassetid://4483345998",
+ Time = 5
+})
+
+_G.Key = "OMGitzAdan"
+_G.KeyInput = "string"
+
+function MakeScriptHub()
+ local Window = local msg = Instance.new("Message",workspace)
+msg.Text = "IT'S A PRANK"
+wait(19999292992999)
+msg:Destroy()
+
+end
+
+local Tab = Window:MakeTab({
+ Name = "Key",
+ Icon = "rbxassetid://4483345998",
+ PremiumOnly = false
+})
+
+Tab:AddTextbox({
+ Name = "Enter Key",
+ Default = "",
+ TextDisappear = true,
+ Callback = function(Value)
+  _G.KeyInput = Value
+ end   
+})
+
+Tab:AddButton({
+ Name = "Submit",
+ Callback = function()
+        if _G.KeyInput == _G.Key then
+        MakeScriptHub()
+        end
+   end    
+})
+
+local Tab = Window:MakeTab({
+ Name = "Get Key",
+ Icon = "rbxassetid://4483345998",
+ PremiumOnly = false
+})
+
+Tab:AddButton({
+ Name = "Copy Key Link",
+ Callback = function()
+setclipboard("https://link-center.net/1003806/get-lightninghub-key1")
+ toclipboard("https://link-center.net/1003806/get-lightninghub-key1")
+   end    
+})
